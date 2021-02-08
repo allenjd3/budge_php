d19dc456a0f7074077a3df79c1ddb6ed69337478
James Allen <jacques2186@yahoo.com>
2021-02-07 19:29:16 -0500

    wip

--- app/Actions/CreateTransactionByMonthAction.php
+++ app/Actions/CreateTransactionByMonthAction.php
@@ -48,6 +48,9 @@ class CreateTransactionByMonthAction extends Action
             $transactions = Transaction::where('month_id','=', $month->id)->whereHas('item', function($query) use($filter){
                 $query->where('name', 'LIKE', '%' . $filter . '%');
             })->with('item')->latest()->paginate(20);
+            if(!$transactions->count()){
+                return redirect('/create-transaction/' . $month->id)->with('message', 'There are no transactions with that filter.');
+            }
         }
         else {
             $transactions = Transaction::where('month_id','=', $month->id)->with('item')->latest()->paginate(20);
--- resources/js/Components/ItemComponent.vue
+++ resources/js/Components/ItemComponent.vue
@@ -3,13 +3,13 @@
     <div class="w-40 md:flex-1 ml-4">
       <a href="" @click.prevent="$emit('toggleModal')">{{name}}</a>
     </div>
-    <div class="w-16 md:w-32 font-bold text-blue-600">${{formattedPlanned}}</div>
+    <div class="w-16 md:w-32 text-sm sm:text-base font-bold text-blue-600">${{formattedPlanned}}</div>
     
     <template v-if="formattedSpent >= 0">
-      <div class="w-16 md:w-32 font-bold text-green-600">${{formattedSpent}}</div>
+      <div class="w-16 md:w-32 text-sm sm:text-base font-bold text-green-600">${{formattedSpent}}</div>
     </template>
     <template v-if="formattedSpent < 0">
-      <div class="w-16 md:w-32 font-bold text-red-600">${{formattedSpent}}</div>
+      <div class="w-16 md:w-32 text-sm sm:text-base font-bold text-red-600">${{formattedSpent}}</div>
     </template>
   </div>
 </template>
